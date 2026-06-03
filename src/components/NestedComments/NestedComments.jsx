170cd79da2e669e312b505e211406415f61c0cb9
Shashank <shashankkp02@gmail.com>
2026-06-03 17:51:52 +0530

    feat: implement state management and handler for adding new nested comments

--- src/components/NestedComments/NestedComments.jsx
+++ src/components/NestedComments/NestedComments.jsx
@@ -1,13 +1,36 @@
+import { useState } from "react";
+
+// function Comment({comment, idx})
+
 export default function CommentsSection() {
+  const newComment = () => {
+    console.log("new comment");
+    comments.push({
+      id: Math.random(),
+      text: "",
+      edit: true,
+      level: 0,
+    });
+  };
+
+  const [comments, setComments] = useState([]);
+
   return (
     <div className="p-4">
       <div className="flex justify-between align-center">
         <h1>Nested Comments</h1>
-        <button className="p-2 bg-blue-500 rounded-lg h-10 cursor-pointer text-white">
+        <button
+          className="p-2 bg-blue-500 rounded-lg h-10 cursor-pointer text-white"
+          onClick={() => {
+            newComment();
+          }}
+        >
           New Comment
         </button>
       </div>
       <p>comments will appear here</p>
+
+      <div>{comments.map((comment, idx) => {})}</div>
     </div>
   );
 }
